4b5277ced2a48ade55408fa418fd52fe309b0eec
Sierra Buchanan <Sierrabuchanan97@gmail.com>
2021-09-07 22:07:14 -0400

    first commit

---
+++ src/main/java/helloworld/Helloworld.java
@@ -0,0 +1,83 @@
+package helloworld;
+
+public class Helloworld {
+    //Main method -black box brain
+    public static void main(String[] args) {
+        System.out.println("My first line of java code using intellij");
+
+        //Primitive data types
+//     byte aBite = 120;
+//     System.out.println(aBite);
+//    int myNum = 5;
+//    float myFloatNum = 5.99f;
+//    char myLetter = 'Y';
+//    boolean mybool = false;
+//    String myText = "yyyy"
+        //Class Practice
+//    byte avar = 123 - 40;
+//    System.out.print(avar);
+//
+//    short anvar = 33000 / 15;
+//    System.out.print(anvar/15);
+//
+//    int hella = 500000000;
+//    System.out.print(hella);
+//
+//    long numm = 40000;
+//    System.out.print(numm);
+//
+//    float aah = 6.7f;
+//    System.out.print (aah);
+//
+//    double team = 15.777;
+//    System.out.print(team);
+//
+//    boolean gang = false;
+//    System.out.print(gang);
+//
+//    char hehe = 'Y';
+//    System.out.print(hehe);
+
+        //MiniLab
+//    double grill = 14.5;
+//    System.out.print(grill);
+
+//    int p =50; double q = 2.0;
+//    var result = p * q;
+//    System.out.print(result);
+
+//        String firstname = "Cece";
+//        String lastname = "Buchanan";
+//        String fullname = firstname + " - " + lastname;
+//        System.out.print(fullname);
+        //What will print out?
+//        String hundredbucks = "100";
+//        String fiftyBucks = "50";
+//        System.out.print(hundredbucks + fiftyBucks);
+        //How to Print out numbers
+//        int hundredBucks = 100;
+//        int fiftyBucks = 50;
+//        System.out.println(hundredBucks + fiftyBucks);
+        //dataType variableName = value
+//            String userName = "Cece";
+//            System.out.print(userName);
+       //String Method for the letter "i"
+        String s = "Yo , chill";
+        System.out.println(s.charAt(7));
+
+        int len = s.length();
+
+        char theFifthChar = s.charAt(4);
+
+        char theLastChar = s.charAt(s.length() - 1);
+
+        System.out.print("yo".equals("no"));
+
+
+
+    }
+
+
+}
+
+
